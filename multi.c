83bde484b2651cbc9ce0db9129bc1de22a12e20d
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-09 16:22:42 +0530

    Create multi.c

---
+++ multi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,i;
+printf("enter number:");
+scanf("%d",&a);
+printf("enter range");
+scanf("%d",&b);
+for(i=0;i<=b;++i)
+{
+printf("%d * %d =%d\n",a,i,a*i);
+getch();
+return 0;
+}
